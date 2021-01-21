8b2e93e9e887dfb53f232bcd0828431a2e6db8fb
beary <bearyme@outlook.com>
2021-01-21 17:49:58 +0800

    change config

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    outDir: 'docs',
+    base: ''
+  }
 })
